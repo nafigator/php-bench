2a2f6efdd184a98f6ce08cb81e1502eb73d88de2
Alexander Yancharuk <alex@itvault.info>
2013-08-24 14:24:41 +0400

    New test for speed check of "if" and "and"

---
+++ Tests/IfVsAnd.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Check what is faster if or and
+ * @file    IfVsAnd.php
+ *
+ * PHP version 5.3.9+
+ *
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ * @date    2013-08-24 14:01
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Tests;
+
+use Veles\Tools\CliProgressBar;
+use Veles\Tools\Timer;
+
+/**
+ * Class IfVsAnd
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ */
+class IfVsAnd extends TestApplication
+{
+	protected static $repeats = 10000;
+
+	final public static function run()
+	{
+		$repeats = self::getRepeats();
+
+		$var = true;
+		$tmp = false;
+
+		$bar = new CliProgressBar($repeats);
+		for ($i = 0; $i <= $repeats; ++$i) {
+			Timer::start();
+			if ($var) {
+				$tmp = $var;
+			}
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('if', Timer::get());
+
+		$bar = new CliProgressBar($repeats);
+
+		Timer::reset();
+		for ($i = 0; $i <= $repeats; ++$i) {
+			Timer::start();
+			$var and $tmp = $var;
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('and', Timer::get());
+	}
+}
