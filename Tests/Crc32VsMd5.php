9fda6b2ca1e2bdefa08665ce68cbdd2d54b332ae
Alexander Yancharuk <alex@itvault.info>
2013-08-28 17:24:31 +0400

    New test for check speed of crc32 and md5 algorithms

---
+++ Tests/Crc32VsMd5.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Check what is faster - crc32 or md5 hashing algorithms
+ * @file    Crc32VsMd5.php
+ *
+ * PHP version 5.3.9+
+ *
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ * @date    Сбт Фев 16 17:01:16 2013
+ * @copyright The BSD 3-Clause License.
+ */
+
+namespace Tests;
+
+use Veles\Tools\CliProgressBar;
+use Veles\Tools\Timer;
+
+/**
+ * Class Crc32VsMd5
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ */
+class Crc32VsMd5 extends TestApplication
+{
+	const TEST = 'simple value';
+	protected static $repeats = 1000;
+
+	final public static function run()
+	{
+        $string = 'some random data';
+
+		$repeats = self::getRepeats();
+		$bar = new CliProgressBar($repeats);
+		for ($i = 0; $i <= $repeats; ++$i) {
+			Timer::start();
+			dechex(crc32($string));
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('dechex(crc32())', Timer::get());
+
+		$bar = new CliProgressBar($repeats);
+		Timer::reset();
+		for ($i = 0; $i <= $repeats; ++$i) {
+			Timer::start();
+			hash('crc32b', $string);
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('hash(\'crc32b\')', Timer::get());
+
+        $bar = new CliProgressBar($repeats);
+        Timer::reset();
+        for ($i = 0; $i <= $repeats; ++$i) {
+            Timer::start();
+            md5($string);
+            Timer::stop();
+            $bar->update($i);
+        }
+
+        self::addResult('md5()', Timer::get());
+	}
+}
