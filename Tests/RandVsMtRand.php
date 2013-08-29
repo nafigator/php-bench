109ad2ab93c76c29baa0f4c568f9390de463869f
Alexander Yancharuk <alex@itvault.info>
2013-08-29 11:23:12 +0400

    Added new test for speed checking rand and mt_rand

---
+++ Tests/RandVsMtRand.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Check what is faster rand() or mt_rand()
+ * @file    RandVsMtRand.php
+ *
+ * PHP version 5.3.9+
+ *
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ * @date    2013-08-04 10:16
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Tests;
+
+use Veles\Tools\CliProgressBar;
+use Veles\Tools\Timer;
+
+/**
+ * Class RandVsMtRand
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ */
+class RandVsMtRand extends TestApplication
+{
+	protected static $repeats = 10000;
+
+	final public static function run()
+	{
+		$repeats = self::getRepeats();
+
+		$bar = new CliProgressBar($repeats);
+		for ($i = 0; $i <= $repeats; ++$i) {
+			Timer::start();
+			rand();
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('rand', Timer::get());
+
+		$bar = new CliProgressBar($repeats);
+
+		Timer::reset();
+		for ($i = 0; $i <= $repeats; ++$i) {
+			Timer::start();
+			mt_rand();
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('mt_rand', Timer::get());
+	}
+}
