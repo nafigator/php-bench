6f572220cf83aab0900b0a5c6285e2d10453cf58
Alexander Yancharuk <alex@itvault.info>
2013-12-31 17:39:26 +0400

    Added test for performance of array search functions

---
+++ Tests/ArraySearchVsInArray.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @todo <Test description here>
+ * @file    ArraySearchVsInArray.php
+ *
+ * PHP version 5.3.9+
+ *
+ * @author  Alexander Yancharuk <alex@itvault.info>
+ * @date    Tue Dec 31 17:30:46 2013
+ * @copyright The BSD 3-Clause License.
+ */
+
+namespace Tests;
+
+use Veles\Tools\CliProgressBar;
+use Veles\Tools\Timer;
+use Application\TestApplication;
+
+/**
+ * Class ArraySearchVsInArray
+ *
+ * @author Alexander Yancharuk <alex@itvault.info>
+ */
+class ArraySearchVsInArray extends TestApplication
+{
+    protected static $repeats = 10000;
+
+	final public static function run()
+	{
+		$haystack = array(
+			'apple',
+			'banana',
+			'cherry',
+			'potato',
+			'rutabaga'
+		);
+		$needle = 'rutabaga';
+
+		$repeats = self::getRepeats();
+
+		$bar = new CliProgressBar($repeats);
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			array_search($needle, $haystack);
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('array_search', Timer::get());
+
+		Timer::reset();
+		$bar = new CliProgressBar($repeats);
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			in_array($needle, $haystack);
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('in_array', Timer::get());
+	}
+}
