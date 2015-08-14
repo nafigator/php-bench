5198f08409772abcaed897c1e09077b275c0245c
Yancharuk Alexander <alex@itvault.info>
2015-08-14 18:06:09 +0300

    Added test for fastest increment

---
+++ Tests/FastestIncrement.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @todo <Test description here>
+ * @file    FastestIncrement.php
+ *
+ * PHP version 5.3.9+
+ *
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ * @date    Fri Aug 14 18:02:18 2015
+ * @copyright The BSD 3-Clause License.
+ */
+
+namespace Tests;
+
+use Veles\Tools\CliProgressBar;
+use Veles\Tools\Timer;
+use Application\TestApplication;
+
+/**
+ * Class FastestIncrement
+ *
+ * @author Yancharuk Alexander <alex@itvault.info>
+ */
+class FastestIncrement extends TestApplication
+{
+    protected static $repeats = 10000;
+
+	final public static function run()
+	{
+		$repeats = self::getRepeats();
+
+		$bar = new CliProgressBar($repeats);
+		$x = 0;
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			$x = $x + 1;
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('$x = $x + 1', Timer::get());
+
+		Timer::reset();
+		$bar = new CliProgressBar($repeats);
+		$x = 0;
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			$x += 1;
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('$x += 1', Timer::get());
+
+		Timer::reset();
+		$bar = new CliProgressBar($repeats);
+		$x = 0;
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			$x++;
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('$x++', Timer::get());
+
+		Timer::reset();
+		$bar = new CliProgressBar($repeats);
+		$x = 0;
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			++$x;
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		self::addResult('++$x', Timer::get());
+	}
+}
