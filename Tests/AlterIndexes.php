399f01541604d22793e567ad848476e9e089ed20
Yancharuk Alexander <alex@itvault.info>
2018-07-31 13:38:56 +0300

    Updated AlterIndex script

--- Tests/AlterIndexes.php
+++ Tests/AlterIndexes.php
@@ -15,6 +15,7 @@
 
 namespace Tests;
 
+use PDO;
 use Veles\DataBase\Db;
 use Veles\Tools\CliProgressBar;
 use Veles\Tools\Timer;
@@ -36,6 +37,7 @@ class AlterIndexes extends TestApplication
 	public function run()
 	{
 		$repeats = $this->getRepeats();
+		$this->prepareDataBase();
 
 		Timer::reset();
 		$bar = new CliProgressBar($repeats);
@@ -62,5 +64,54 @@ class AlterIndexes extends TestApplication
 		}
 
 		$this->addResult('insert + key', Timer::get());
+	}
+
+    protected function prepareDataBase()
+    {
+//        $dbh = new PDO("mysql:host=localhost", 'root', '');
+//        $dbh->exec("CREATE DATABASE IF NOT EXISTS landings");
+//
+//        $dbh->exec("
+//          CREATE TABLE IF NOT EXISTS `united_countries` (
+//           `id` int(11) NOT NULL AUTO_INCREMENT,
+//           `name` varchar(255) NOT NULL,
+//           `code` varchar(255) NOT NULL,
+//           `description` text,
+//           PRIMARY KEY (`id`)
+//         ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8");
+//
+//        $dbh->exec("
+//            INSERT INTO `united_countries` VALUES
+//            (7,'Россия','RU',NULL),
+//            (6,'Украина лидовая','UA_lead',NULL),
+//            (5,'Испания','ES',NULL),
+//            (4,'Польша','PL',NULL),
+//            (3,'Казахстан','KZ',NULL),
+//            (2,'Единая Россия под авто','',NULL),
+//            (1,'Украина','UA',NULL)
+//        ");
+//
+//        $dbh->exec("
+//           CREATE TABLE IF NOT EXISTS `united_requests` (
+//          `id` int(11) NOT NULL AUTO_INCREMENT,
+//          `country_id` int(11) NOT NULL,
+//          `affiliate_id` int(10) unsigned NOT NULL,
+//          `created_date` date NOT NULL,
+//          `created_datetime` datetime NOT NULL,
+//          `status` tinyint(4) NOT NULL,
+//          `phone` varchar(32) DEFAULT NULL,
+//          `email` varchar(255) DEFAULT NULL,
+//          `incoming_data` text NOT NULL,
+//          `error_code` int(4) DEFAULT NULL,
+//          `error_message` varchar(1024) DEFAULT NULL,
+//          `ip` varchar(40) DEFAULT NULL,
+//          PRIMARY KEY (`id`),
+//          KEY `phone_index` (`phone`),
+//          KEY `date` (`created_date`),
+//          KEY `countries` (`country_id`),
+//          KEY `phone_date` (`phone`,`created_date`),
+//          KEY `wmid_date` (`created_date`,`country_id`,`affiliate_id`),
+//          CONSTRAINT `country_fk` FOREIGN KEY (`country_id`) REFERENCES `united_countries` (`id`)
+//        ) ENGINE=InnoDB AUTO_INCREMENT=153958338 DEFAULT CHARSET=utf8");
 	}
 }
