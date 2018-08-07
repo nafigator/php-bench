b99f4dca4d5c6114d76469a87ccf95fe29dd449c
Yancharuk Alexander <alex@itvault.info>
2018-08-07 19:11:02 +0300

    Added test for double and single quotes performance

---
+++ Tests/DoubleVsSingleQuotes.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @todo <Test description here>
+ * @file      DoubleVsSingleQuotes.php
+ *
+ * PHP version 5.6+
+ *
+ * @author    Yancharuk Alexander <alex at itvault dot info>
+ * @copyright © 2013-2018 Yancharuk Alexander
+ * @date      Tue Aug 07 19:05:30 2018
+ * @license   The BSD 3-Clause License.
+ *            <https://tldrlegal.com/license/bsd-3-clause-license-(revised)>
+ */
+
+namespace Tests;
+
+use Veles\Tools\CliProgressBar;
+use Veles\Tools\Timer;
+use Application\TestApplication;
+
+/**
+ * Class DoubleVsSingleQuotes
+ *
+ * @author Yancharuk Alexander <alex at itvault dot info>
+ */
+class DoubleVsSingleQuotes extends TestApplication
+{
+    protected $repeats = 10000;
+
+	public function run()
+	{
+		$repeats = $this->getRepeats();
+
+		Timer::reset();
+		$bar = new CliProgressBar($repeats);
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			$a = 'qwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdf';
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		$this->addResult('Single', Timer::get());
+
+		Timer::reset();
+		$bar = new CliProgressBar($repeats);
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			$a = "qwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdfqwertyasdf";
+			Timer::stop();
+			$bar->update($i);
+		}
+
+		$this->addResult('Double', Timer::get());
+	}
+}
