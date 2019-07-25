36e3ed9558144a9f2f6065e66e1a769e3b7bc150
Yancharuk Alexander <alex@itvault.info>
2019-07-25 16:50:11 +0300

    Style fix

--- Tests/Fpm.php
+++ Tests/Fpm.php
@@ -30,46 +30,46 @@ use Veles\Tools\Timer;
  */
 class Fpm extends TestApplication
 {
-    protected $repeats = 1000;
-    protected const FPM_QUERY = 'http://gateways.local/gateways/sovkom/send?fname=Тест&mname=Тест&lname=Тест&sum=15000&region=Ставропольский Край&city=Пятигорск&work_st=работает&bdate=1993-01-28&chan=testChan&apiKey=1q2w3e4r5t6y7u8i9o0p&wmid=1488&phone=9876543210&email=big_5336@rambler.ru&data1=data1&data2=data2&data3=data3&data4=data4&data5=data5&personal_data_agree=1&personal_data_datetime=2019-07-24 18:12:23&personal_data_url=https://example.com/anketa&advert_agree=1&user_agent=Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0&ip=127.0.0.1';
-    protected const CLI_QUERY = 'http://gateways.local/gateways/mig/send?fname=Тест&mname=Тест&lname=Тест&sum=15000&region=Ставропольский Край&city=Пятигорск&work_st=работает&bdate=1993-01-28&chan=testChan&apiKey=1q2w3e4r5t6y7u8i9o0p&wmid=1488&phone=9876543210&email=big_5336@rambler.ru&data1=data1&data2=data2&data3=data3&data4=data4&data5=data5&personal_data_agree=1&personal_data_datetime=2019-07-24 18:12:23&personal_data_url=https://example.com/anketa&advert_agree=1&user_agent=Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0&ip=127.0.0.1';
-    protected const FPM_QUERY_DATA = [
-        'fname'                  => 'Тест',
-        'mname'                  => 'Тест',
-        'lname'                  => 'Тест',
-        'sum'                    => 15000,
-        'region'                 => 'Ставропольский Край',
-        'city'                   => 'Пятигорск',
-        'work_st'                => 'работает',
-        'bdate'                  => '1993-01-28',
-        'chan'                   => 'testChan',
-        'apiKey'                 => '1q2w3e4r5t6y7u8i9o0p',
-        'wmid'                   => 1488,
-        'phone'                  => 9876543210,
-        'email'                  => 'big_5336@rambler.ru',
-        'data1'                  => 'data1',
-        'data2'                  => 'data2',
-        'data3'                  => 'data3',
-        'data4'                  => 'data4',
-        'data5'                  => 'data5',
-        'personal_data_agree'    => 1,
-        'personal_data_datetime' => '2019-07-24 18:12:23',
-        'personal_data_url'      => 'https://example.com/anketa',
-        'advert_agree'           => 1,
-        'user_agent'             => 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
-        'ip'                     => '127.0.0.1',
-    ];
+	protected $repeats = 1000;
+	protected const FPM_QUERY = 'http://gateways.local/gateways/sovkom/send?fname=Тест&mname=Тест&lname=Тест&sum=15000&region=Ставропольский Край&city=Пятигорск&work_st=работает&bdate=1993-01-28&chan=testChan&apiKey=1q2w3e4r5t6y7u8i9o0p&wmid=1488&phone=9876543210&email=big_5336@rambler.ru&data1=data1&data2=data2&data3=data3&data4=data4&data5=data5&personal_data_agree=1&personal_data_datetime=2019-07-24 18:12:23&personal_data_url=https://example.com/anketa&advert_agree=1&user_agent=Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0&ip=127.0.0.1';
+	protected const CLI_QUERY = 'http://gateways.local/gateways/mig/send?fname=Тест&mname=Тест&lname=Тест&sum=15000&region=Ставропольский Край&city=Пятигорск&work_st=работает&bdate=1993-01-28&chan=testChan&apiKey=1q2w3e4r5t6y7u8i9o0p&wmid=1488&phone=9876543210&email=big_5336@rambler.ru&data1=data1&data2=data2&data3=data3&data4=data4&data5=data5&personal_data_agree=1&personal_data_datetime=2019-07-24 18:12:23&personal_data_url=https://example.com/anketa&advert_agree=1&user_agent=Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0&ip=127.0.0.1';
+	protected const FPM_QUERY_DATA = [
+		'fname'                  => 'Тест',
+		'mname'                  => 'Тест',
+		'lname'                  => 'Тест',
+		'sum'                    => 15000,
+		'region'                 => 'Ставропольский Край',
+		'city'                   => 'Пятигорск',
+		'work_st'                => 'работает',
+		'bdate'                  => '1993-01-28',
+		'chan'                   => 'testChan',
+		'apiKey'                 => '1q2w3e4r5t6y7u8i9o0p',
+		'wmid'                   => 1488,
+		'phone'                  => 9876543210,
+		'email'                  => 'big_5336@rambler.ru',
+		'data1'                  => 'data1',
+		'data2'                  => 'data2',
+		'data3'                  => 'data3',
+		'data4'                  => 'data4',
+		'data5'                  => 'data5',
+		'personal_data_agree'    => 1,
+		'personal_data_datetime' => '2019-07-24 18:12:23',
+		'personal_data_url'      => 'https://example.com/anketa',
+		'advert_agree'           => 1,
+		'user_agent'             => 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+		'ip'                     => '127.0.0.1',
+	];
 
 	public function run()
 	{
-	    ini_set('display_errors', true);
-	    ini_set('display_startup_errors', true);
-	    ini_set('log_errors', true);
-	    ini_set('error_log', '/var/log/php_errors.log');
+		ini_set('display_errors', true);
+		ini_set('display_startup_errors', true);
+		ini_set('log_errors', true);
+		ini_set('error_log', '/var/log/php_errors.log');
 		$repeats = $this->getRepeats();
 
-        $sender = new Sender;
-        $sender->parseQuery(static::FPM_QUERY);
+		$sender = new Sender;
+		$sender->parseQuery(static::FPM_QUERY);
 
 		Timer::reset();
 		$bar = new CliProgressBar($repeats);
@@ -84,34 +84,34 @@ class Fpm extends TestApplication
 
 		Timer::reset();
 
-        $sender->parseQuery(static::CLI_QUERY);
+		$sender->parseQuery(static::CLI_QUERY);
 
 		$bar = new CliProgressBar($repeats);
 		for ($i = 1; $i <= $repeats; ++$i) {
 			Timer::start();
-            $sender->sendFromQueue();
+			$sender->sendFromQueue();
 			Timer::stop();
 			$bar->update($i);
 		}
 
 		$this->addResult('cgi-fcgi', Timer::get());
 
-        Timer::reset();
+		Timer::reset();
 
-        $connection = new UnixDomainSocket('/run/php/alex-fpm.sock');
+		$connection = new UnixDomainSocket('/run/php/alex-fpm.sock');
 
-        $content = http_build_query(static::FPM_QUERY_DATA);
-        $request = new GetRequest('/home/alex/dev/gateways/public/index.php', $content);
-        $client = new Client;
+		$content = http_build_query(static::FPM_QUERY_DATA);
+		$request = new GetRequest('/home/alex/dev/gateways/public/index.php', $content);
+		$client = new Client;
 
-        $bar = new CliProgressBar($repeats);
-        for ($i = 1; $i <= $repeats; ++$i) {
-            Timer::start();
-            $client->sendRequest($connection, $request);
-            Timer::stop();
-            $bar->update($i);
-        }
+		$bar = new CliProgressBar($repeats);
+		for ($i = 1; $i <= $repeats; ++$i) {
+			Timer::start();
+			$client->sendRequest($connection, $request);
+			Timer::stop();
+			$bar->update($i);
+		}
 
-        $this->addResult('FPM lib', Timer::get());
+		$this->addResult('FPM lib', Timer::get());
 	}
 }
